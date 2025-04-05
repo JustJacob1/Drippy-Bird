54c212f814ef486240ab682ca190f6b1caa050e9
Jacob Le <jacoble-coderschoolpasadena@outlook.com>
2025-04-05 13:56:40 -0700

    Advancements
    I was able to add the main character and make it jump with space bar and left click. I also added all the images required to help make this game fun to play.

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 import pygame
 pygame.init()
 screen = pygame.display.set_mode((400,600))
-screen.fill("white")
+clock = pygame.time.Clock()
+
+player = pygame.image.load('Photos/West.png').convert_alpha()
+player = pygame.transform.smoothscale(player, [100,100])
+player = player.subsurface(player.get_bounding_rect())
+player_pos = pygame.Vector2(0,225)
+y_axis = 0
+
 
 is_running =  True
 while is_running:
+    jump = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
-            pygame.quit()
+            quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                jump = True
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                jump = True
 
+    y_axis += .25
+    if jump:
+        y_axis = -5
+    player_pos.y += y_axis
+    screen.fill("white")
+    screen.blit(player, player_pos)
     pygame.display.flip()
+    clock.tick(60)

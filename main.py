933d4bd980cba4f1094357f072e40b10d2250975
JustJacob1 <jacoble-coderschoolpasadena@outlook.com>
2025-04-26 14:00:17 -0700

    Pipes
    I started to work on pipes for my flappybird game

--- main.py
+++ main.py
@@ -9,20 +9,9 @@ player = pygame.transform.smoothscale(player, [100,100])
 player = player.subsurface(player.get_bounding_rect())
 player_pos = pygame.Vector2(0,225)
 y_axis = 0
-backround = pygame.image.load
-pipe = pygame.image.load("Pipe.png")
-pipe_width = pipe.get_width()
-pipe_height = pipe.get_height()
-
-#Pipes
-pipe_gap = 150
-pipe_distance = 200
-pipe_speed = 2
-pipes = []
-for i in range(3):
-    x = 400 + pipe_distance * 1
-    y_offset = random.randint(-150, 0)
-    pipes.append({"x": x, "y_offset": y_offset})
+backround = pygame.image.load("Photos/bg.png").convert_alpha()
+backround = pygame.transform.scale(backround, [400,600])
+backround = backround.get_rect()
 
 is_running =  True
 while is_running:
@@ -45,16 +34,7 @@ while is_running:
         y_axis = -5
 
     player_pos.y += y_axis
-    for pipe_data in pipes:
-        pipe_data["x"] -= pipe_speed
-
-        if pipe_data["x"] <-pipe_width
-            pipe_data['x'] = max(p["x"])
-            pipe_data["y_offset"] = random.randint(-150,0)
-        backround.blit([pipe,(pipe_data["x"], pipe_data"y_offset"])
-        bottom_pipe = pygame.transform.flip(pipe,False,True)
-        bottom_y = pipe_data["y_offset"] + pipe_height + pipe_gap
-        backround.blit(bottom_pipe(pipe_data["x"], bottom_y))
+    
 
 
     screen.fill("white")

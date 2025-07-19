ea856ea3cb42ae81a08c5529b5b9fd6d971535fd
JustJacob1 <jacoble-coderschoolpasadena@outlook.com>
2025-07-19 14:11:55 -0700

    rock
    i came back from vacation, but was able to make loops

--- Astroid_boy.py
+++ Astroid_boy.py
@@ -1,4 +1,5 @@
 import pygame
+from space_rock import Asteroid
 
 # pygame setup
 pygame.init()
@@ -11,6 +12,18 @@ backround_image = pygame.transform.scale(backround_image, [1500,800])
 backround_rect = backround_image.get_rect()
 Astroid = pygame.image.load("Photos/Astroid.png").convert_alpha()
 Astroid = pygame.transform.scale(Astroid, [80,80])
+
+astroid_list = []
+for _ in range(10):
+    asteroid = Asteroid(Astroid, 1500, 800)
+    astroid_list.append(asteroid)
+
+
+
+
+
+
+
 
 player = pygame.Vector2(1800, 1100)/2
 Angle = 220
@@ -28,7 +41,7 @@ while running:
     speed*= 0.7
     player += speed.rotate(Angle)
     screen.blit(backround_image, backround_rect)
-    screen.blit(Astroid)
+    screen.blit(Astroid, backround_rect)
     pygame.draw.circle(screen, "white", player, 15)
     pygame.draw.line(screen, "white", player, player+pygame.Vector2(20, 0).rotate(Angle))
     # RENDER YOUR GAME HERE
---
+++ space_rock.py
@@ -0,0 +1,16 @@
+import pygame
+import random
+
+class Asteroid:
+    def __init__(self, image, screen_width, screen_height):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0, screen_width - self.rect.width)
+        self.rect.y = random.randint(-200, -50)
+        self.speed = random.randint(2,6)
+    
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+    def move(self):
+        self.rect.y += self.speed
